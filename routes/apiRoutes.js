66c5420f461c8077addf05d87e6012b5e66ae63e
AnthonyKuehl <anthony.kuehl.sanitz@outlook.de>
2023-04-16 20:06:18 +0200

    LEVMV

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,27 +22,9 @@ router.get('/tabelle/:liga', async (req, res) => {
     res.json(table)
 })
 
-router.get('/game/:id/:home/:away', async (req, res) => {
-    const teamNameAway = req.params.away + " " + req.params.id;
-    const teamNameHome = req.params.home + " " + req.params.id;
-    res.render('gamedetail', { 
-        home: await Aufstellung.findOne({ teamName: teamNameHome }), 
-        away: await Aufstellung.findOne({ teamName: teamNameAway }), 
-        data: await Model.findById(req.params.id), 
-        liga: liga, 
-        goals: await Goal.find({ gameID: req.params.id }), 
-        penaltys: await Penalty.find({ gameID: req.params.id }).sort({time: 1}),
-        goalsHome: await Goal.find({ gameID: req.params.id, verein: req.params.home }),
-        goalsAway: await Goal.find({ gameID: req.params.id, verein: req.params.away }),
-        goalsHome1: await Goal.find({ gameID: req.params.id, verein: req.params.home, period: 1 }),
-        goalsHome2: await Goal.find({ gameID: req.params.id, verein: req.params.home, period: 2 }),
-        goalsHome3: await Goal.find({ gameID: req.params.id, verein: req.params.home, period: 3 }),
-        goalsAway1: await Goal.find({ gameID: req.params.id, verein: req.params.away, period: 1 }),
-        goalsAway2: await Goal.find({ gameID: req.params.id, verein: req.params.away, period: 2 }),
-        goalsAway3: await Goal.find({ gameID: req.params.id, verein: req.params.away, period: 3 }),
-        goalsHomeOT: await Goal.find({ gameID: req.params.id, verein: req.params.home, period: 4 }),
-        goalsAwayOT: await Goal.find({ gameID: req.params.id, verein: req.params.away, period: 4 })
-    })
+router.get('/game/:id', async (req, res) => {
+    let game = await Model.findById(req.params.id)
+    res.json(game)
 })
 
 module.exports = router
